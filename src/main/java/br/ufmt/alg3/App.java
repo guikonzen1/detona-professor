e2f9031bdfd9c406904a45bba3da35e7ad7c8cae
guikonzen1 <guilhermekonzen1@gmail.com>
2024-07-31 21:32:45 -0400

    Demostra a utilização das referências

--- src/main/java/br/ufmt/alg3/App.java
+++ src/main/java/br/ufmt/alg3/App.java
@@ -1,6 +1,8 @@
 package br.ufmt.alg3;
 
 
+import java.util.Scanner;
+
 import br.ufmt.alg3.curso.Curso;
 import br.ufmt.alg3.professor.Professor;
 
@@ -9,39 +11,28 @@ public class App
 {
    public static void main( String[] args )
    {
-       Curso c = new Curso("SI");
-       Curso c2 = new Curso();
-       c2.setNota(0.f);
-       c2.setPeriodo("Vespertino");
-       System.out.println(c.getNome());
-       System.out.println(c.getPeriodo());
-       System.out.println("-----------------------");
+       Curso[] respostas;
+       respostas = new Curso[3];
 
+       //int[][] mat = new int[2][3];//Criar matriz em java
 
-       System.out.println(c2.getNome());
-       System.out.println(c2.getNota());
-       System.out.println(c2.getPeriodo());
-       System.out.println(c2.getPeriodo());
+       Scanner teclado;
+       teclado = new Scanner(System.in);
 
+     
 
+       for(int i = 0; i < respostas.length; i++){
+       Curso c = new Curso();
+       System.out.print("Digite o nome do curso: ");
+       String nome = teclado.nextLine();
+       
+       c.setNome(nome);
+       respostas[i] = c;
+       }
 
-
-       Professor p = new Professor(); // Usa new pra instanciar um novo objeto a uma variável
-       p.setNome("Raphael");
-       p.setareaAtuacao("TI");
-       p.setdisciplina("Algoritmos III");
-       p.setSemestre("2024/1");
-
-
-       Professor p2 = new Professor();
-       p2.setNome("OutroNome");
-
-
-       Professor p3 = new Professor();
-       Professor p4 = p3;
-       p3 = p;
-       //4 objetos e 5 variáveis
-       System.out.println(p.getNome());
+       for (Curso curso : respostas) {
+        System.out.println("Curso avaliado: " + curso.getNome());
+       }
 
 
    }
